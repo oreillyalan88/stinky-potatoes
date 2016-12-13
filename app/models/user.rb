cfd243b7b292d3cdd616acde03336dcdd296a389
Alan O' Reilly <oreillyalan88@gmail.com>
2016-12-13 18:48:12 +0000

    Review Scaffold

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   
   has_many :movies
+  has_many :reviews, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
